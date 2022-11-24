1b8a608e26e202b7a2e668a5fd42a14553f42c39
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2022-11-24 19:42:43 +0800

    navbar with js

--- assignment/index.js
+++ assignment/index.js
@@ -1,1 +1,13 @@
+const hamburger = document.querySelector('.fa-bars');
+const navbar = document.querySelector('.navbar');
+const close = document.querySelector('.fa-xmark');
 
+hamburger.addEventListener('click', () => {
+  navbar.style.display = 'flex';
+  close.style.display = 'flex';
+});
+
+close.addEventListener('click', () => {
+  navbar.style.display = 'none';
+  close.style.display = 'none';
+});
